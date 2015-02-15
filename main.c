256d4cb36d7f12d66c932ebc1c427cb17b90f678
mnmoretti <manuel.moretti@tiscali.it>
2015-02-15 16:50:25 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,47 +1,45 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int potenzaIterativa(int base, int esponente);
-int potenzaRicorsiva(int base, int esponente);
-
-int main(int argc, char** argv) {
-    int base,esponente;
-    int rIterativo, rRicorsivo;
-    
-    printf("Inserisci la base (intera): ");
-    scanf("%d", &base);
-    printf("Inserisci l'esponente (intero non ngeativo): ");
-    scanf("%d", &esponente);
-    
-    rIterativo = potenzaIterativa(base, esponente);
-    rRicorsivo = potenzaRicorsiva(base, esponente);
-    
-    printf("%d^%d = %d (formulazione iterativa)\n", base, esponente, rIterativo);
-    printf("%d^%d = %d (formulazione ricorsiva)\n", base, esponente, rRicorsivo);
-    return (EXIT_SUCCESS);
-}
-
-int potenzaIterativa(int base, int esponente) {
-    int risultato = 1;
-    int i;
-    
-    for(i=0;i<esponente;i++) {
-        risultato *= base;
-    }
-    
-    return risultato;
-}
-int potenzaRicorsiva(int base, int esponente) {
-    int risultato = 1;
-    
-    if(esponente!=0) {
-        risultato = base * potenzaRicorsiva(base,esponente-1);
-    }else {
-        return risultato;
-    }
-}
-int potenzaIterativa(int base, int esponente) {
-    // TODO Implementa il corpo della funzione
-    return -1;
+#include <stdio.h> 
+#include <stdlib.h> 
+ 
+ 
+ int potenzaIterativa(int base, int esponente); 
+ int potenzaRicorsiva(int base, int esponente); 
+ 
+ 
+ int main(int argc, char** argv) { 
+     int base,esponente; 
+     int rIterativo, rRicorsivo; 
+      
+     printf("Inserisci la base (intera): "); 
+     scanf("%d", &base); 
+     printf("Inserisci l'esponente (intero non ngeativo): "); 
+     scanf("%d", &esponente); 
+      
+     rIterativo = potenzaIterativa(base, esponente); 
+     rRicorsivo = potenzaRicorsiva(base, esponente); 
+      
+     printf("%d^%d = %d (formulazione iterativa)\n", base, esponente, rIterativo); 
+     printf("%d^%d = %d (formulazione ricorsiva)\n", base, esponente, rRicorsivo); 
+     return (EXIT_SUCCESS); 
+ } 
+ 
+ 
+ int potenzaIterativa(int base, int esponente) { 
+     int risultato = 1; 
+     int i; 
+      
+     for(i=0;i<esponente;i++) { 
+         risultato *= base; 
+     } 
+      
+     return risultato; 
+ } 
+ int potenzaRicorsiva(int base, int esponente) { 
+     int risultato = 1; 
+      
+     if(esponente!=0) { 
+         risultato = base * potenzaRicorsiva(base,esponente-1); 
+     }else { 
+         return risultato; 
+     } 
 }
